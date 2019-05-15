b8edd32f128f0367d965cfa58317ea2dae9e6c20
Hitoshi AFUSO <hitoshiafuso@gmail.com>
2019-05-15 10:46:29 +0900

    Implemented first version of skeleton extraction

--- test.py
+++ test.py
@@ -10,29 +10,33 @@ mecab = MeCab.Tagger("/usr/lib/mecab/dic")
 with open("./dataset/H29_hakase.csv") as f:
     reader = csv.reader(f)
     data = [row for row in reader]
-row_count = int(sys.argv[1])
-parse_results = mecab.parse(data[row_count][9])
+# row_count = int(sys.argv[1])
+# parse_results = mecab.parse(data[row_count][9])
 
-noun = []
-conj = []
-keywords = []
-skeleton = []
-noun_count = 0
-for res in parse_results.split('\n'):
-    cols = res.split('\t')
-    if(1 < len(cols)):
-        parts = cols[1].split(',')
-        if(parts[0].startswith('名詞')):
-            noun.append(cols[0])
-            noun_count += 1
-        elif(parts[0].startswith('接続詞')):
-            conj.append(cols[0])
-            skeleton.append(noun_count)
-            skeleton.append(cols[0])
-            noun_count = 0
-        if(parts[0].startswith('名詞') or parts[0].startswith('接続詞')):
-            keywords.append(cols[0])
-skeleton.append(noun_count)
+for i in range(1, len(data)):
 
-pdb.set_trace()
+    parse_results = mecab.parse(data[i][9])
+
+    noun = []
+    conj = []
+    keywords = []
+    skeleton = []
+    noun_count = 0
+    for res in parse_results.split('\n'):
+        cols = res.split('\t')
+        if(1 < len(cols)):
+            parts = cols[1].split(',')
+            if(parts[0].startswith('名詞')):
+                noun.append(cols[0])
+                noun_count += 1
+            elif(parts[0].startswith('接続詞')):
+                conj.append(cols[0])
+                skeleton.append(noun_count)
+                skeleton.append(cols[0])
+                noun_count = 0
+            if(parts[0].startswith('名詞') or parts[0].startswith('接続詞')):
+                keywords.append(cols[0])
+    skeleton.append(noun_count)
+    print(data[i][0] + "\t" + str(skeleton) + "\t" + data[i][12] + "\t" + data[i][13])
+    # pdb.set_trace()
 
